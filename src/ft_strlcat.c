9ab73131858ab8414ed85314321756d29d6a5131
Vandre Raia <vraia@e1z3r3p7.42.us.org>
2018-10-26 21:00:12 -0700

    fix strlcat

--- src/ft_strlcat.c
+++ src/ft_strlcat.c
@@ -6,7 +6,7 @@
 /*   By: vraia <marvin@42.fr>                       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/10/25 22:24:24 by vraia             #+#    #+#             */
-/*   Updated: 2018/10/25 22:32:05 by vraia            ###   ########.fr       */
+/*   Updated: 2018/10/26 20:59:31 by vraia            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,18 +16,22 @@ size_t	ft_strlcat(char *dst, const char *src, size_t size)
 {
 	size_t i;
 	size_t j;
+	size_t end;
 
 	i = 0;
 	j = 0;
+	if (size <= ft_strlen(dst))
+		return (ft_strlen(src) + size);
 	while (dst[i] && i < size)
 		i++;
+	end = i;
 	while (src[j] && i < size)
 	{
 		dst[i] = src[j];
 		i++;
 		j++;
 	}
-	if (i < size)
+	if (end < size)
 		dst[i] = '\0';
-	return (i);
+	return (end + strlen(src));
 }

51973ed6bed90a8da16447f397a3a9febf6a498c
Vandre Raia <vraia@e1z2r8p32.42.us.org>
2018-11-22 19:57:23 -0800

    add get next line

---
+++ include/get_next_line.h
@@ -0,0 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: vraia <marvin@42.fr>                       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2018/11/07 13:12:49 by vraia             #+#    #+#             */
+/*   Updated: 2018/11/22 19:56:03 by vraia            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+# include "libft.h"
+# include <stdlib.h>
+# include <unistd.h>
+
+# define BUFF_SIZE 4
+
+int		get_next_line(const int fd, char **line);
+
+#endif
